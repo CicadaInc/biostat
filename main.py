6d8e78752540d3b984d24807e67739ccaccd695d
Mark <mark.2406@yandex.ru>
2018-12-03 16:43:07 +0500

    working main window

---
+++ main.py
@@ -0,0 +1,24 @@
+import sys
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QLabel
+from PyQt5.QtGui import QPixmap
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.fone = QLabel(self)
+        self.fone.resize(800, 500)
+        self.fone.move(0, 0)
+        self.fone.setPixmap(QPixmap("image.jpg").scaled(800, 500))
+        uic.loadUi('main.ui', self)
+        self.pushButton.clicked.connect(self.run)
+
+    def run(self):
+        self.label.setText("OK")
+
+
+app = QApplication(sys.argv)
+ex = MyWidget()
+ex.show()
+sys.exit(app.exec_())
