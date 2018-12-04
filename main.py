4cd1d54e470d84bc520dffc89d218991a64c5955
Mark <mark.2406@yandex.ru>
2018-12-04 09:08:19 +0500

    add depend of windows (program structure)

--- main.py
+++ main.py
@@ -1,34 +1,66 @@
 import sys
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QLabel
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
 from PyQt5.QtGui import QPixmap
 
+'''Основной класс (от него идут все окна программы)'''
 
-class MyWidget(QMainWindow):
+
+class BioStat(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.fone = QLabel(self)
-        self.fone.resize(800, 500)
-        self.fone.move(0, 0)
-        self.fone.setPixmap(QPixmap("image.jpg").scaled(800, 500))
+        self.mainWin = MainMenu()
+        self.hide()
+
+    def starting(self):
+        self.startWin = StartWindow()
+
+
+'''Основное меню'''
+
+
+class MainMenu(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.init_UI()
+        self.show()
+
+    def init_UI(self):
+        set_fone(self)
 
         uic.loadUi('main.ui', self)
 
         self.pushStart.clicked.connect(self.starting)
 
     def starting(self):
-        global window
-        window = MyWidget1()
-        window.show()
+        global prog
+
+        self.hide()
+        BioStat.starting(prog)  # Запуск окна старта из основного класса
 
 
-class MyWidget1(QMainWindow):
+'''Окно при нажатии на старт'''
+
+
+class StartWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.init_UI()
+        self.show()
+
+    def init_UI(self):
+        set_fone(self)
+
         uic.loadUi('main.ui', self)
 
 
+def set_fone(self):  # Установка фона для окон
+    self.fone = QLabel(self)
+    self.fone.resize(800, 500)
+    self.fone.move(0, 0)
+    self.fone.setPixmap(QPixmap("image.jpg").scaled(800, 500))
+
+
 app = QApplication(sys.argv)
-ex = MyWidget()
-ex.show()
+prog = BioStat()
 sys.exit(app.exec_())
