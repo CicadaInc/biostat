f1c95029d1f99dfb14482507c3eea57088554edc
Dmitriy <dmitriykuznetsov23@gmail.com>
2018-12-04 16:09:03 +0500

    new design, comments and structure

--- main.py
+++ main.py
@@ -1,34 +1,74 @@
 import sys
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QLabel
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
 from PyQt5.QtGui import QPixmap
 
 
-class MyWidget(QMainWindow):
+# Main class
+class BioStat(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.fone = QLabel(self)
-        self.fone.resize(800, 500)
-        self.fone.move(0, 0)
-        self.fone.setPixmap(QPixmap("image.jpg").scaled(800, 500))
+        self.mainWin = MainMenu()
+        self.hide()
 
+    def starting(self, main):
+        self.startWin = StartWindow()
+        main.hide()
+
+
+# Main menu
+class MainMenu(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.init_UI()
+        self.show()
+
+    def init_UI(self):
+        # Установка фона
+        set_fone(self)
+
+        # Загрузка GUI
         uic.loadUi('main.ui', self)
 
+        # Подкличение функционала к кнопкам
         self.pushStart.clicked.connect(self.starting)
 
     def starting(self):
-        global window
-        window = MyWidget1()
-        window.show()
+        global prog
+        BioStat.starting(prog, self)  # Запуск окна старта из основного класса
 
 
-class MyWidget1(QMainWindow):
+# Window start
+class StartWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('main.ui', self)
+        self.init_UI()
+        self.show()
+
+    def init_UI(self):
+        # Установка фона
+        set_fone(self)
+
+        # Загрузка GUI
+        uic.loadUi('start.ui', self)
+
+        # Подкличение функционала к кнопкам
+        self.pushStart.clicked.connect(self.starting)
+
+    def searching(self):  # Search button
+        pass
+
+    def backToMain(self):  # Back to main window button
+        pass
+
+
+def set_fone(self):  # Установка фона для окон
+    self.fone = QLabel(self)
+    self.fone.resize(900, 600)
+    self.fone.move(0, 0)
+    self.fone.setPixmap(QPixmap("image.jpg").scaled(901, 600))
 
 
 app = QApplication(sys.argv)
-ex = MyWidget()
-ex.show()
+prog = BioStat()
 sys.exit(app.exec_())
