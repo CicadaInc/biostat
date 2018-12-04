a952047f6616273512998cb19f2c1a138a28c0b5
Mark <mark.2406@yandex.ru>
2018-12-04 18:01:28 +0500

    depends of all windows

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ class BioStat(QMainWindow):
         super().__init__()
         self.mainWin = MainMenu()
         self.startWin = StartWindow()
+        self.statisticWin = StatisticWindow()
+        self.progInfo = ProgramInformation()
 
 
 # Main menu
@@ -22,18 +24,32 @@ class MainMenu(QMainWindow):
 
     def init_UI(self):
         # Установка фона
-        set_fone(self)
+        set_background(self)
 
         # Загрузка GUI
         uic.loadUi('main.ui', self)
 
         # Подкличение функционала к кнопкам
         self.pushStart.clicked.connect(self.starting)
+        self.pushStatistic.clicked.connect(self.show_statistic)
+        self.pushAbout.clicked.connect(self.show_program_info)
 
     def starting(self):
         global prog
 
         prog.startWin.show()  # Запуск окна старта
+        self.hide()
+
+    def show_statistic(self):
+        global prog
+
+        prog.statisticWin.show()  # Запуск окна статистики
+        self.hide()
+
+    def show_program_info(self):
+        global prog
+
+        prog.progInfo.show()
         self.hide()
 
 
@@ -46,28 +62,56 @@ class StartWindow(QMainWindow):
 
     def init_UI(self):
         # Установка фона
-        set_fone(self)
+        set_background(self)
 
         # Загрузка GUI
         uic.loadUi('start.ui', self)
 
-        self.pushBack.clicked.connect(self.back_to_main)
+        self.pushBackFromStart.clicked.connect(lambda: back_to_main(self))
 
     def searching(self):  # Search button
         pass
 
-    def back_to_main(self):  # Back to main window button
-        global prog
 
-        prog.mainWin.show()  # Запуск окна старта
-        self.hide()
+class StatisticWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setFixedSize(800, 500)
+        self.init_UI()
+
+    def init_UI(self):
+        set_background(self)
+
+        uic.loadUi('statistic.ui', self)
+
+        self.pushBackFromStatistic.clicked.connect(lambda: back_to_main(self))
 
 
-def set_fone(self):  # Установка фона для окон
-    self.fone = QLabel(self)
-    self.fone.resize(900, 600)
-    self.fone.move(0, 0)
-    self.fone.setPixmap(QPixmap("image.jpg").scaled(901, 600))
+class ProgramInformation(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setFixedSize(800, 500)
+        self.init_UI()
+
+    def init_UI(self):
+        set_background(self)
+
+        uic.loadUi('program_info.ui', self)
+
+        self.pushBackFromInfo.clicked.connect(lambda: back_to_main(self))
+
+
+def set_background(self):  # Установка фона для окон
+    self.bg = QLabel(self)
+    self.bg.resize(900, 600)
+    self.bg.setPixmap(QPixmap("image.jpg").scaled(900, 600))
+
+
+def back_to_main(self):  # Back to main window button
+    global prog
+
+    prog.mainWin.show()  # Запуск окна старта
+    self.hide()
 
 
 app = QApplication(sys.argv)
